ac0163e2f8c0667249655444d9fa9c23a66afb94
Nat Sharpe <sharpe1890@gmail.com>
2018-08-14 10:19:50 -0400

    created file which uses dns to lookup IP Address based on inputted domain name

---
+++ DNS_lookup.js
@@ -0,0 +1,26 @@
+var dns = require('dns');
+var readline = require('readline');
+
+var readlineInterface = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+var lookupDNS = function (url) {
+    dns.lookup(url, function(error, address) {
+        var errorMessage = '\nError: Domain does not exist.\n';
+        var successMessage =  `\nDomain Name: ${url}\nIP Address: ${address}\n`;
+
+        if (error) {
+            console.log(errorMessage);
+        }
+        else {
+            console.log(successMessage);
+        }
+    });
+};
+
+readlineInterface.question('What website would you like to lookup? ', function(answer) {
+    lookupDNS(answer);
+    readlineInterface.close();
+});
